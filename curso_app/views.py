0d94545498b161bb1288b0a07910d1cd84d8ca22
Raphael andrade <Raphaelsilk@gmail.com>
2024-04-02 11:59:55 -0300

    mudança de titulo

--- curso_app/views.py
+++ curso_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 def home(request):
    dados = {
-      "nome": "Sistema de cadastro"
+      "nome": "Cadastro de cursos Cyber Dev"
    }
    return render (request,"curso_app/home.html", dados)
 
